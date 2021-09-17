464c88b1c671e7570d1a2f85de1d984cf609ce7f
Buhaienko-Kate <buguzia89@gmail.com>
2021-09-17 22:31:49 +0300

    change webpack

--- Lesson6/task1/webpack.config.js
+++ Lesson6/task1/webpack.config.js
@@ -5,21 +5,25 @@ module.exports = {
   },
   module: {
     rules: [
+      // {
+      //   test: /\.css$/,
+      //   use: [
+      //     // style-loader
+      //     { loader: 'style-loader' },
+      //     // css-loader
+      //     {
+      //       loader: 'css-loader',
+      //       options: {
+      //         modules: true,
+      //       },
+      //     },
+      //     // sass-loader
+      //     { loader: 'sass-loader' },
+      //   ],
+      // },
       {
-        test: /\.css$/,
-        use: [
-          // style-loader
-          { loader: 'style-loader' },
-          // css-loader
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-            },
-          },
-          // sass-loader
-          { loader: 'sass-loader' },
-        ],
+        test: /.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /.(jpg|png)$/,
@@ -27,7 +31,7 @@ module.exports = {
           {
             loader: 'url-loader',
             options: {
-              limit: 8,
+              limit: 8192,
               name: '[name].[ext]',
               outputPath: 'images',
             },
