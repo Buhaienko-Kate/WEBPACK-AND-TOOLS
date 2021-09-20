8dd46596775b93f023a2191f8cd5683243e147ee
Buhaienko-Kate <buguzia89@gmail.com>
2021-09-20 12:07:30 +0300

    added lesson8

---
+++ Lesson8/task2/src/list/updateTasks.js
@@ -0,0 +1,38 @@
+import { renderTasks } from './renderer.js';
+import { listElem } from './createTask.js';
+import { setItem, getItem } from './storage.js';
+import { updateTask, getTasksList } from './tasksGateWay.js';
+
+export const updatedTasks = event => {
+  const isCheckbox = event.target.classList.contains('list__item-checkbox');
+
+  if (!isCheckbox) {
+    return;
+  }
+  const checkedElem = event.target;
+
+  const tasksList = getItem('tasksList');
+  const taskId = checkedElem.dataset.id;
+  const { text } = tasksList.find(task => task.id === taskId);
+
+  const updatedTask = {
+    text,
+    done: checkedElem.checked,
+  };
+
+  updateTask(taskId, updatedTask)
+    .then(() => getTasksList())
+    .then(newTasksList => {
+      setItem(
+        'tasksList',
+        newTasksList,
+      ); /**полученные данные с сервера записываем в LocalStorage */
+      renderTasks();
+    });
+};
+
+// 1.Prepare data
+// 2. update data to db
+// 3.Read new data from server
+// 4. Save new data to front-end storage
+// 5. Update
